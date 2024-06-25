952c32f160f00e2b879f16d1398eb6b99e6a8e7b
Amr Ahmed <amroahmed50@outlook.com>
2024-06-25 17:32:19 +0300

    fix?

--- express_API/src/applicants/router.ts
+++ express_API/src/applicants/router.ts
@@ -13,7 +13,10 @@ const fileFilter = (req: any, file: any, cb: any) => {
     cb(null, true);
   } else {
     // Reject the file
-    cb(new Error('Invalid file type'), false);
+    cb(
+      new multer.MulterError('LIMIT_UNEXPECTED_FILE', 'Invalid file type'),
+      false,
+    );
   }
 };
 const upload = multer({
